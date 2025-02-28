df9171db34c2a2158cd492bddc40f2ca1291c154
bugholic <masterav2001@gmail.com>
2025-02-28 20:04:16 +0530

    project complete

---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+import Approach from "@/components/Approach";
+import Client from "@/components/Client";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
+import Grid from "@/components/Grid";
+import Hero from "@/components/Hero";
+import RecentProject from "@/components/recent-project";
+import { FloatingNav } from "@/components/ui/floating-nav";
+import { navItems } from "@/data";
+import Image from "next/image";
+import { FaHome } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav
+          navItems={navItems}
+        />
+        <Hero />
+        <Grid />
+        <RecentProject />
+        <Client />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
+  );
+}
